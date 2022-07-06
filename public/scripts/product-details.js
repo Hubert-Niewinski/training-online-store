52dc6b42ed04fa07d2efcd770fe235effcd3283f
Hubert Niewinski <niewinskihubert@gmail.com>
2022-07-06 22:43:07 +0200

    chore: remove duplicated script code

--- public/scripts/product-details.js
+++
@@ -1,7 +0,0 @@
-const backButtonElement = document.getElementById("back-btn");
-
-async function backToProductPage() {
-  return window.history.back();
-}
-
-backButtonElement.addEventListener("click", backToProductPage);
